5654b5749cb802b8ec7c06f2211f0a155c70c363
CHEN Fangting <cft0627@gmail.com>
2024-03-04 23:50:55 +0800

    Updated day 22 exercise

--- day22.py
+++ day22.py
@@ -58,3 +58,69 @@ if rows: #If there are rows
         row_number += 1
 else:
     print("No rows found in the table.")
+
+print(f'Exercise 1'.center(80, "-"))
+print('Scrape the following website and store the data as json file(url = \'http://www.bu.edu/president/boston-university-facts-stats/\').')
+
+import requests
+import json
+import webbrowser
+
+url = 'http://www.bu.edu/president/boston-university-facts-stats/'
+response = requests.get(url)
+print(response.status_code)
+# webbrowser.open_new_tab(url) #in any default webbrowser
+
+content = response.content
+soup = BeautifulSoup(content, 'html.parser') 
+#Here soup is an instance/object. 
+#syntax: soup = BeautifulSoup(html_doc, 'html.parser')
+
+print(soup.title)  #<title>BU Facts &amp; Stats | Office of the President</title>
+print(soup.title.get_text())  #BU Facts & Stats | Office of the President
+# print(soup.body)
+
+print('\nstep1')
+# Find the section containing Quick Facts & Stats
+facts_section = soup.find('section', class_='facts-stats') #Subsection
+
+if facts_section:
+    # Extract data from hexagons
+    hexagons = facts_section.find_all('div', class_='hexagon') #Sub-Subsection 1
+    
+    hexagon_data = []
+    for hexagon in hexagons:
+        value = hexagon.find('h2').text.strip()
+        label = hexagon.find('h4').text.strip()
+        hexagon_data.append({'label': label, 'value': value})
+
+    # Extract data from facts categories
+    facts_categories = soup.find_all('div', class_='facts-wrapper') #Sub-Subsection 2
+    # print(facts_categories) #This row used to check whether scrap the subsubsection successfully
+
+    facts_data = {}
+    for category in facts_categories:
+        category_name = category.find('h5').text.strip()
+        category_items = category.find_all('li', class_='list-item')
+        
+        category_values = {}
+        for item in category_items:
+            text = item.find('p', class_='text').text.strip()
+            value = item.find('span', class_='value').text.strip()
+            category_values[text] = value
+
+        facts_data[category_name] = category_values
+
+    # Combine both sets of data
+    result_data = {'hexagons': hexagon_data, 'categories': facts_data}
+
+    # Convert the data to JSON
+    json_text = json.dumps(result_data, indent=2, ensure_ascii=False)
+
+    # Save the JSON data to a file
+    with open('boston_university_facts.json', 'w', encoding='utf-8') as json_file:
+        json_file.write(json_text)
+
+    print("Data saved as 'boston_university_facts.json'")
+else:
+    print("Facts section not found on the webpage.")
